d03c5df17ca30483b532afb77d7c69dd5c22e4b0
ExorTek <exortek979@gmail.com>
2021-04-12 16:01:57 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = '-';
+const prefix = '!';
 const fs = require('fs');
 
 client.commands = new Discord.Collection();
@@ -47,4 +47,4 @@ client.on('message', message => {
     }
 });
 
-client.login('ODI4NzAwNjc5OTI0NzQ0MjM0.YGtZpg.v6VLjLyuOcDhobNhExPyRFMHCt4');
+client.login('ODI4NzAwNjc5OTI0NzQ0MjM0.YGtZpg.B1RVwcVYY3hmm9Ma-hvqvWtYVyA');
